97875e9b85cd20590a799ead24b8a74c5dd802b8
jaiber <antoniogarcia200603@gmail.com>
2023-09-15 16:20:58 -0500

    Clase de constantes

---
+++ src/main/java/com/example/pruebatecnica/util/Constante.java
@@ -0,0 +1,17 @@
+package com.example.pruebatecnica.util;
+
+/**
+ * Clase Constantes
+ *
+ * @author Jaiber Antonio
+ */
+public class Constante {
+
+    /** endpoints */
+
+    public static final  String PATH_AUDITORIAS = "/auditorias";
+
+    /** Mensajes de error */
+    public static final String ERROR_ID_NO_ENCONTRADO= "Auditoria no encontrada con ID: ";
+    public static final String ERROR_ACTUALUZAR = "no se pudo actualizar";
+}
