50636e014627e0d26cbe69a791a6a6bc45501c6f
jaiber <antoniogarcia200603@gmail.com>
2023-09-15 16:10:33 -0500

    actualizacion AuditorioController

--- src/main/java/com/example/pruebatecnica/controller/AuditoriasController.java
+++ src/main/java/com/example/pruebatecnica/controller/AuditoriasController.java
@@ -3,13 +3,19 @@ package com.example.pruebatecnica.controller;
 import com.example.pruebatecnica.dto.AuditoriaDTO;
 import com.example.pruebatecnica.entity.AuditoriasEntity;
 import com.example.pruebatecnica.service.impl.ImpAuditoriaService;
+import com.example.pruebatecnica.util.Constante;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Clases controller de auditorias
+ * @author
+ */
 @RestController
-@RequestMapping("/auditorias")
+@RequestMapping(Constante.AUDITORIAS)
+@CrossOrigin(originPatterns = "http://localhost:4200")
 public class AuditoriasController {
 
     @Autowired
@@ -19,14 +25,31 @@ public class AuditoriasController {
     public void update (@PathVariable Long idAuditorias, @RequestBody AuditoriasEntity auditoriasEntity){
         impAuditoriaService.updateAuditoria(idAuditorias,auditoriasEntity);
     }
+    @PostMapping("/crear")
+    public AuditoriaDTO create (@RequestBody AuditoriasEntity auditoriasEntity){
+        return impAuditoriaService.createAuditoria(auditoriasEntity);
+    }
 
     @DeleteMapping("/borrar/{idAuditorias}")
     public void delete (@PathVariable Long idAuditorias ){
         impAuditoriaService.deleteAuditoriaById(idAuditorias);
     }
 
+    @GetMapping("/traerpornombre/{nombre}")
+    public AuditoriaDTO findByNombre (@PathVariable String nombre){
+    return impAuditoriaService.findBynombre(nombre);
+
+    }
+    @GetMapping("/traerporid/{idAuditorias}")
+    public AuditoriaDTO findById (@PathVariable Long idAuditorias){
+        return impAuditoriaService.findById(idAuditorias);
+    }
     @GetMapping("/traer")
-    public List<AuditoriaDTO> traer (){
+    public List<AuditoriaDTO> get (){
         return impAuditoriaService.obtenerAuditorias();
+    }
+    @DeleteMapping("/borrarpornombre/{nombre}")
+    public void delete (@PathVariable String nombre){
+        impAuditoriaService.deleteAuditoriaByNombre(nombre);
     }
 }
